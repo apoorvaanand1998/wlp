62258f77d82b51410c4cf72e38ec86ce5db45f6b
Apoorva Anand <apoorvaanand.work@gmail.com>
2023-09-18 14:40:59 +0200

    Typo

--- src/WLP.hs
+++ src/WLP.hs
@@ -13,7 +13,7 @@ wlp (GCLD.Seq s1 s2) post        = wlp s1 (wlp s2 post)
 wlp (GCLD.IfThenElse _ _ _) _    = error "IfThenElse is a Compound Statement being passed to WLP"
 wlp (GCLD.While _ _) _           = error "While is a Compound Statement being passed to WLP"
 -- TODO
--- wlp (GCLD.AAsign _ _ _) post     = Repby --something
+-- wlp (GCLD.AAssign _ _ _) post     = Repby --something
 -- wlp (GCLD.DrefAssign _ _) post      = -- wtf
 -- wlp (GCLD.Block _ _) _ = -- convert to something else and then call WLP on that?
 -- wlp (GCLD.TryCatch _ _ _) _ = -- do we go with method 1 or 2?
