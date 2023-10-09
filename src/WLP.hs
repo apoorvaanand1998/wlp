7a7742fdc87bec4a9aa2b74a118fbe2f3ff63487
Apoorva Anand <apoorvaanand.work@gmail.com>
2023-10-09 12:33:03 +0200

    More cases of Assign

--- src/WLP.hs
+++ src/WLP.hs
@@ -1,6 +1,7 @@
 module WLP where
 
 import qualified GCLParser.GCLDatatype as GCLD
+import qualified GHC.Generics as GCLD
 
 wlp :: GCLD.Stmt -> GCLD.Expr -> GCLD.Expr
 wlp GCLD.Skip post               = post
@@ -22,5 +23,23 @@ wlp (GCLD.Block _ s) post        = wlp s post
 -- wlp (GCLD.TryCatch _ _ _) _ = -- do we go with method 1 or 2?
 
 convertRepByExpr :: GCLD.Expr -> GCLD.Expr
-convertRepByExpr (GCLD.RepBy _ _ _) = undefined
+convertRepByExpr (GCLD.RepBy _ _ (GCLD.RepBy _ _ _)) = undefined -- AAssign
+--Assign
+convertRepByExpr (GCLD.RepBy ve@(GCLD.Var varExpr) (GCLD.Var v) e) = if varExpr == v then e else ve
+convertRepByExpr (GCLD.RepBy li@(GCLD.LitI _) _ _) = li
+convertRepByExpr (GCLD.RepBy lb@(GCLD.LitB _) _ _) = lb
+convertRepByExpr (GCLD.RepBy ln@GCLD.LitNull _ _)  = ln
+convertRepByExpr (GCLD.RepBy (GCLD.Parens pe) v e) = convertRepByExpr (GCLD.RepBy pe v e)
+convertRepByExpr (GCLD.RepBy ae@(GCLD.ArrayElem _ _) _ _) = ae -- i think
+convertRepByExpr (GCLD.RepBy (GCLD.OpNeg ne) v e)  = GCLD.OpNeg (convertRepByExpr (GCLD.RepBy ne v e))
+convertRepByExpr (GCLD.RepBy (GCLD.BinopExpr op be1 be2) v e) = GCLD.BinopExpr op (convertRepByExpr (GCLD.RepBy be1 v e))
+                                                                                  (convertRepByExpr (GCLD.RepBy be2 v e))
+convertRepByExpr (GCLD.RepBy fa@(GCLD.Forall fv fe) vr@(GCLD.Var v) e) = if fv /= v 
+                                                           then GCLD.Forall fv (convertRepByExpr (GCLD.RepBy fe vr e))
+                                                           else fa
+convertRepByExpr (GCLD.RepBy ex@(GCLD.Exists ev ee) vr@(GCLD.Var v) e) = if ev /= v 
+                                                           then GCLD.Exists ev (convertRepByExpr (GCLD.RepBy ee vr e))
+                                                           else ex
+                                                           
 convertRepByExpr _                  = error "Non RepBy Expr trying to be substituted"
+
