58e7e3af3aa118b5017553167c912480040c0388
Wilco Verhoef <git@wilcoverhoef.nl>
2023-10-11 02:05:57 +0200

    printing the tree

--- src/Paths.hs
+++ src/Paths.hs
@@ -1,6 +1,7 @@
-module Paths (programTree) where
+module Paths (Tree(..), Statement(..), programTree, limitDepth) where
 
 import GCLParser.GCLDatatype
+import Data.List (intercalate)
 
 -- Probably need to annotate this eventually to have useful output
 data Statement
@@ -9,44 +10,87 @@ data Statement
     | SAssign String Expr
     | SAAssign String Expr Expr
 
-newtype Tree a = Tree [Node a] deriving (Semigroup, Monoid, Functor, Show)
-data Node a = Node a (Tree a) deriving (Functor, Show)
+instance Show Statement where
+  show (SAssert expr) = unwords ["ASSERT", show expr]
+  show (SAssume expr) = unwords ["ASSUME", show expr]
+  show (SAssign var expr) = unwords ["ASSIGN", var, show expr]
+  show (SAAssign arr idx expr) = unwords ["AASSIGN", arr, show idx, show expr]
 
--- an alternative Semigroup instance
--- instance Semigroup (Tree a) where
---   Tree xss <> ys = Tree $ map (\(Node x xs) -> Node x (xs <> ys)) xss
+newtype Tree a = Tree [Node a] deriving (Semigroup, Monoid, Functor)
+data Node a = Node a (Tree a) deriving (Functor)
 
--- instance Applicative Tree where
---   pure x = Tree [pure x]
---   Tree fs <*> Tree xs = Tree $ liftA2 (<*>) fs xs
+instance Show a => Show (Tree a) where
+  show (Tree []) = "\n└─■"
+  show (Tree [n])    = (("\n└── " ++) . intercalate "\n    " . lines) (show n)
+  show (Tree (n:ns)) = (("\n├── " ++) . intercalate "\n│   " . lines) (show n) ++ show (Tree ns)
 
--- instance Applicative Node where
---   pure x = Node x mempty
---   Node f fs <*> b@(Node x xs) = Node (f x) ((f <$> xs) <> (fs <*> Tree [b]))
+instance Show a => Show (Node a) where
+  show (Node x t) = show x ++ show t
 
+limitDepth :: Int -> Tree a -> Tree a
+limitDepth 0 _ = Tree []
+limitDepth k (Tree xs) = Tree (map f xs)
+  where f (Node n ns) = Node n (limitDepth (k-1) ns)
 
 programTree :: Program -> Tree Statement
-programTree Program { input, stmt } = tree (Block input stmt) mempty
+programTree Program { input, output, stmt } = tree 0 (Block (input ++ output) stmt) mempty
 
 -- Insert a new root above the current root of the tree
 (|>) :: a -> Tree a -> Tree a
 x |> t = Tree [Node x t]
 
-tree :: Stmt -> Tree Statement -> Tree Statement
-tree Skip = id
-tree (Assert cond) = (SAssert cond |>)
-tree (Assume cond) = (SAssume cond |>)
-tree (Assign var expr) = (SAssign var expr |>)
-tree (AAssign arr idx expr) = (SAAssign arr idx expr |>)
-tree (DrefAssign _var _expr) = error "out of scope?"
-tree (Seq expr1 expr2) = tree expr1 . tree expr2
-tree (IfThenElse guard true false)
-  = (SAssume guard         |>) . tree true
- <> (SAssume (OpNeg guard) |>) . tree false
-tree (While guard body)
+tree :: Int -> Stmt -> Tree Statement -> Tree Statement
+tree _ Skip = id
+tree _ (Assert cond) = (SAssert cond |>)
+tree _ (Assume cond) = (SAssume cond |>)
+tree _ (Assign var expr) = (SAssign var expr |>)
+tree _ (AAssign arr idx expr) = (SAAssign arr idx expr |>)
+tree _ (DrefAssign _var _expr) = error "out of scope?"
+tree i (Seq expr1 expr2) = tree i expr1 . tree i expr2
+tree i (IfThenElse guard true false)
+  = (SAssume guard         |>) . tree i true
+ <> (SAssume (OpNeg guard) |>) . tree i false
+tree i (While guard body)
   = (SAssume (OpNeg guard) |>)
- <> (SAssume guard |>) . tree body . tree (While guard body)
-tree (Block xs stmt) = foldMap f xs . tree stmt
-  where f :: VarDeclaration -> Tree Statement -> Tree Statement
-        f = error "todo: what to do with var declaration?"
-tree (TryCatch _catch _try _expr) = error "out of scope?"
+ <> (SAssume guard |>) . tree i body . tree i (While guard body)
+tree i (Block [] stmt) = tree i stmt
+tree i (Block (VarDeclaration s _:xs) stmt) = rename s (varName i) . tree (succ i) (Block xs stmt)
+tree _ (TryCatch _catch _try _expr) = error "out of scope?"
+
+varName :: Int -> String
+varName i = 'x' : map ((digs !!) . read . pure) (show i)
+  where digs = "₀₁₂₃₄₅₆₈₉"
+
+-- problem: when the original program contains any variable with a number as a name...
+-- does that happen?
+rename :: String -> String -> Tree Statement -> Tree Statement
+rename rep by = fmap (renameStmt rep by)
+
+renameStmt :: String -> String -> Statement -> Statement
+renameStmt rep by (SAssert expr) = SAssert $ renameExpr rep by expr
+renameStmt rep by (SAssume expr) = SAssume $ renameExpr rep by expr
+renameStmt rep by (SAssign var expr) | rep == var = SAssign by $ renameExpr rep by expr
+                                     | otherwise  = SAssign var $ renameExpr rep by expr
+renameStmt rep by (SAAssign arr idx expr) | rep == arr = SAAssign by idx $ renameExpr rep by expr
+                                          | otherwise  = SAAssign arr idx $ renameExpr rep by expr
+
+renameExpr :: String -> String -> Expr -> Expr
+renameExpr rep by (OpNeg expr) = OpNeg (renameExpr rep by expr)
+renameExpr rep by (Var var) | rep == var = Var by
+                            | otherwise  = Var var
+renameExpr _ _ l@(LitI _)  = l
+renameExpr _ _ l@(LitB _)  = l
+renameExpr _ _ l@(LitNull) = l
+renameExpr rep by (Parens expr) = Parens (renameExpr rep by expr)
+renameExpr rep by (ArrayElem expr1 expr2) = ArrayElem (renameExpr rep by expr1) (renameExpr rep by expr2)
+renameExpr rep by (BinopExpr op expr1 expr2) = BinopExpr op (renameExpr rep by expr1) (renameExpr rep by expr2)
+renameExpr rep by (Forall var expr) | rep == var = Forall by (renameExpr rep by expr)
+                                    | otherwise  = Forall var (renameExpr rep by expr)
+renameExpr rep by (Exists var expr) | rep == var = Exists by (renameExpr rep by expr)
+                                    | otherwise  = Exists var (renameExpr rep by expr)
+renameExpr rep by (SizeOf expr) = SizeOf (renameExpr rep by expr)
+renameExpr rep by (RepBy expr1 expr2 expr3) = RepBy (renameExpr rep by expr1) (renameExpr rep by expr2) (renameExpr rep by expr3)
+renameExpr rep by (Cond expr1 expr2 expr3) = Cond (renameExpr rep by expr1) (renameExpr rep by expr2) (renameExpr rep by expr3)
+renameExpr rep by (NewStore expr) = NewStore (renameExpr rep by expr)
+renameExpr rep by (Dereference var) | rep == var = Dereference by
+                                    | otherwise  = Dereference var
