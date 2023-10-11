8ad607aa073cf5ef2135336c27dd32ee47045417
Apoorva Anand <apoorvaanand.work@gmail.com>
2023-10-11 08:07:37 +0200

    Some cases of AAssign. Should consider simplification?

--- src/WLP.hs
+++ src/WLP.hs
@@ -10,7 +10,7 @@ wlp (GCLD.Assign varname e) post = GCLD.RepBy post (GCLD.Var varname) e
 wlp (GCLD.Assert e) post         = GCLD.BinopExpr GCLD.And e post
 wlp (GCLD.Assume e) post         = GCLD.BinopExpr GCLD.Implication e post
 wlp (GCLD.Seq s1 s2) post        = wlp s1 (wlp s2 post)
-wlp (GCLD.IfThenElse {}) _    = error "IfThenElse is a Compound Statement being passed to WLP"
+wlp (GCLD.IfThenElse {}) _       = error "IfThenElse is a Compound Statement being passed to WLP"
 wlp (GCLD.While _ _) _           = error "While is a Compound Statement being passed to WLP"
 wlp (GCLD.AAssign var i e) post  = GCLD.RepBy post (GCLD.Var var) (GCLD.RepBy (GCLD.Var var) i e)
 -- RepBy expressions need to be changed to substituted expressions before feeding into z3
@@ -19,11 +19,18 @@ wlp (GCLD.Block _ s) post        = wlp s post
 -- Loops are a special case, rename multiple times in every iteration
 -- TODO
 wlp (GCLD.DrefAssign _ _) _   = error "Dref not implemented in wlp"
-wlp (GCLD.TryCatch {}) _ = error "Try not implemented in wlp" -- do we go with method 1 error "not implemented" -- 2?
+wlp (GCLD.TryCatch {}) _ = error "Try not implemented in wlp" -- do we go with method 1 or 2?
 
 -- function needs clean-up...if we have time
 repBy :: GCLD.Expr -> GCLD.Expr
-repBy (GCLD.RepBy _ _ (GCLD.RepBy _ _ _)) = undefined -- AAssign
+-- AAssign
+-- arr[i] := e
+repBy (GCLD.RepBy var@(GCLD.Var varExpr) v (GCLD.RepBy _ i e)) = var
+repBy (GCLD.RepBy li@(GCLD.LitI _) v (GCLD.RepBy _ i e)) = li
+repBy (GCLD.RepBy lb@(GCLD.LitB _) v (GCLD.RepBy _ i e)) = lb
+repBy (GCLD.RepBy ln@GCLD.LitNull v (GCLD.RepBy _ i e))  = ln
+repBy (GCLD.RepBy (GCLD.Parens pe) v (GCLD.RepBy _ i e)) = repBy (GCLD.RepBy pe v (GCLD.RepBy v i e))
+repBy (GCLD.RepBy (GCLD.ArrayElem ae ai) v (GCLD.RepBy _ i e)) = undefined -- need to simplify first 
 --Assign
 repBy (GCLD.RepBy ve@(GCLD.Var varExpr) (GCLD.Var v) e) = if varExpr == v then e else ve
 repBy (GCLD.RepBy li@(GCLD.LitI _) _ _) = li
