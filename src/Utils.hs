6f65984adbbe60785d9183c0f6ddc2712369d521
Wilco Verhoef <git@wilcoverhoef.nl>
2024-01-10 13:50:13 +0100

    countatoms

--- src/Utils.hs
+++ src/Utils.hs
@@ -2,15 +2,40 @@ module Utils (countAtoms) where
 
 import qualified GCLParser.GCLDatatype as GCLD
 
--- not converting first to dnf/cnf because that's a np-hard problem apparently
+
+
 countAtoms :: GCLD.Expr -> Int
 countAtoms (GCLD.Parens e)   = countAtoms e
-countAtoms (GCLD.OpNeg _)    = 1
+countAtoms (GCLD.OpNeg e)    = countAtoms e
 countAtoms (GCLD.Forall _ e) = countAtoms e
 countAtoms (GCLD.Exists _ e) = countAtoms e
-countAtoms (GCLD.BinopExpr b e1 e2) = 
-                               case b of
-                                    GCLD.And -> 1 + countAtoms e1 + countAtoms e2
-                                    GCLD.Or  -> 1 + countAtoms e1 + countAtoms e2
-                                    _        -> countAtoms e1 + countAtoms e2
-countAtoms _                 = 0
+
+countAtoms (GCLD.BinopExpr binop e1 e2)
+  | boolean binop = countAtoms e1 + countAtoms e2
+  | otherwise     = countAtoms e1 + countAtoms e2 - 1
+
+countAtoms (GCLD.Var _)           = 1
+countAtoms (GCLD.LitB _)          = 1
+countAtoms (GCLD.LitI _)          = 1
+countAtoms GCLD.LitNull           = 1
+countAtoms (GCLD.ArrayElem e1 e2) = countAtoms e1 + countAtoms e2 - 1
+countAtoms (GCLD.SizeOf e)        = countAtoms e
+
+countAtoms (GCLD.RepBy e1 e2 e3)  = countAtoms e1 + countAtoms e2 + countAtoms e3
+countAtoms (GCLD.Cond e1 e2 e3)   = countAtoms e1 + countAtoms e2 + countAtoms e3
+countAtoms (GCLD.NewStore e)      = countAtoms e
+countAtoms (GCLD.Dereference _)   = 1
+
+
+
+boolean :: GCLD.BinOp -> Bool
+boolean GCLD.And = True
+boolean GCLD.Or  = True
+boolean GCLD.Implication = True
+boolean _ = False
+
+
+
+showExpr :: GCLD.Expr -> String
+showExpr (GCLD.BinopExpr op e1 e2) = " B: " ++ show op ++ " {1$" ++ showExpr e1 ++ "$1} " ++ " {2$" ++ showExpr e2 ++ "$2}"
+showExpr e                         = " F: " ++ show e
