32bd4f321407feaf73ba1feccee6455709a2a189
Wilco Verhoef <git@wilcoverhoef.nl>
2024-01-10 15:06:38 +0100

    total formula sizes

--- src/WLP.hs
+++ src/WLP.hs
@@ -28,16 +28,16 @@ treeWLP h tree = runWriter $ do
 verify :: Int ->  Map String Expr -> Expr -> PathTree -> Writer [Metric] Expr
 
 -- Ignore infeasible paths (unsatisfiable assumptions)
-verify _ _ pre _ | not (isFeasible pre) = tell [Path False] >> pure (GCLD.LitB True)
+verify _ _ pre _ | not (isFeasible pre) = tell [Path False, Formula (countAtoms pre)] >> pure (GCLD.LitB True)
 
 -- Base case
-verify _ _ _ Terminate = tell [Path True] >> pure (GCLD.LitB True)
+verify _ _ pre Terminate = tell [Path True, Formula (countAtoms pre)] >> pure (GCLD.LitB True)
 
 -- Depending on the heuristic, either check for feasibility first or calculate wp directly
 verify h vars pre (Stmt s t)
     | atomHeuristic h vars pre s = let (pre', vars') = runState (sp s pre) vars in
-                                   wp s <$> verify h vars' (memoSimplify pre') t
-    | otherwise                  = wp s <$> verify h mempty (GCLD.LitB True) t
+                                   tell [Formula (countAtoms pre)] >> wp s <$> verify h vars' (memoSimplify pre') t
+    | otherwise                  = tell [Formula (countAtoms pre)] >> wp s <$> verify h mempty (GCLD.LitB True) t
 
 -- Branches
 verify h vars pre (Branch t1 t2) = memoSimplify <$> (liftA2 (GCLD.BinopExpr GCLD.And) `on` verify h vars pre) t1 t2
